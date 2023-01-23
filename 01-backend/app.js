bd5a45907823f32efeaddf2774a328d2fa99564d
rachelhmyx <myha1211998@gmail.com>
2023-01-23 20:24:47 +0700

    add new

--- 01-backend/app.js
+++ 01-backend/app.js
@@ -6,6 +6,8 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var categoriesRouter = require("./routes/categories");
+var customersRouter = require("./routes/customers");
 
 var cors = require("cors");
 var app = express();
@@ -28,6 +30,8 @@ app.use(
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/categories", categoriesRouter);
+app.use("/customers", customersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ 01-backend/routes/categories.js
@@ -0,0 +1,59 @@
+const { default: mongoose } = require("mongoose");
+const { Category } = require("../models");
+var express = require("express");
+var router = express.Router();
+
+mongoose.connect("mongodb://127.0.0.1:27017/Moon-Daily");
+
+router.post("/", (req, res, next) => {
+  try {
+    const data = req.body;
+    const newItem = new Category(data);
+    newItem.save().then((result) => {
+      res.send(result);
+    });
+  } catch (error) {
+    res.sendStatus(500);
+    console.log("Error:", error);
+  }
+});
+
+router.get("/", (req, res, next) => {
+  try {
+    Category.find().then((result) => {
+      res.send(result);
+    });
+  } catch (error) {
+    res.sendStatus(500);
+    console.log("Error:", error);
+  }
+});
+
+router.delete("/:id", (req, res, next) => {
+  try {
+    const { id } = req.params;
+    Category.findByIdAndDelete(id).then((result) => {
+      res.send(result);
+    });
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+router.patch("/:id", (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    Category.findByIdAndUpdate(id, data, { new: true })
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+module.exports = router;
