daddcfedd61a4c3f5f8528d89f9d3a5393b456b5
rachelhmyx <myha1211998@gmail.com>
2023-02-11 14:36:12 +0700

    addnew

--- 03-frontend-typescript/admin/src/App.tsx
+++ 03-frontend-typescript/admin/src/App.tsx
@@ -1,24 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FaCloudMoon } from "react-icons/fa";
+import Login from "./pages/Login";
+import ResetPassword from "./pages/ResetPassword";
+import ForgotPassword from "./pages/ForgotPassword";
+import Dashboard from "./pages/Dashboard";
+import Categories from "./pages/Categories";
+import SearchBar from "./components/SearchBar";
+import { Layout } from "antd";
+import SideMenuBar from "./components/SideMenuBar";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+import numeral from "numeral";
+import "numeral/locales/vi";
+import Product from "./pages/Products/product";
+
+numeral.locale("vi");
+
+const { Header, Sider, Content } = Layout;
 
 function App() {
+  const [collapsed, setCollapsed] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+        <Layout>
+          <Sider trigger={null} collapsible collapsed={collapsed} theme="dark">
+            <div className="logo">
+              <h2 className="text-white fs-10 text-center text-uppercase fw-bold py-3 mb-0">
+                <span className="sm-logo">{<FaCloudMoon />}</span>
+                <span className="lg-logo">Moon</span>
+              </h2>
+            </div>
+            <SideMenuBar />
+          </Sider>
+          <Layout className="site-layout">
+            <Header
+              className="site-layout-background d-flex justify-content-between ps-1 pe-5"
+              style={{
+                padding: 0,
+              }}
+            >
+              {React.createElement(
+                collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+                {
+                  className: "trigger",
+                  onClick: () => setCollapsed(!collapsed),
+                }
+              )}
+              <SearchBar />
+            </Header>
+            <Content
+              className="site-layout-background"
+              style={{
+                margin: "24px 16px",
+                padding: 24,
+                minHeight: 280,
+              }}
+            >
+              <Routes>
+                <Route index path="/home" element={<Dashboard />} />
+                <Route path="/categories" element={<Categories />} />
+                <Route path="/product" element={<Product />} />
+                {/* NO MATCH ROUTE */}
+                <Route
+                  path="*"
+                  element={
+                    <main style={{ padding: "1rem" }}>
+                      <p>404 Page not found</p>
+                    </main>
+                  }
+                />
+              </Routes>
+            </Content>
+          </Layout>
+        </Layout>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ 03-frontend-typescript/admin/src/components/SideMenuBar.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { HiUserGroup, HiShoppingBag } from "react-icons/hi";
+import { HiClipboardDocumentList } from "react-icons/hi2";
+import { IoStorefront } from "react-icons/io5";
+import { RiShoppingCartFill } from "react-icons/ri";
+import { AiTwotoneSetting } from "react-icons/ai";
+import { FaUserTie, FaHome } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { Menu } from "antd";
+
+function SideMenuBar() {
+  const navigate = useNavigate();
+  return (
+    <>
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={["home"]}
+        onClick={({ key }) => {
+          navigate("/" + key);
+        }}
+        items={[
+          {
+            key: "home",
+            icon: <FaHome className="fs-4" />,
+            label: "Dashboard",
+          },
+          {
+            key: "products",
+            icon: <RiShoppingCartFill className="fs-4" />,
+            label: "Products",
+          },
+          {
+            key: "categories",
+            icon: <HiClipboardDocumentList className="fs-4" />,
+            label: "Category",
+          },
+          {
+            key: "suppliers",
+            icon: <IoStorefront className="fs-4" />,
+            label: "Supplier",
+          },
+          {
+            key: "orders",
+            icon: <HiShoppingBag className="fs-4" />,
+            label: "Order",
+          },
+          {
+            key: "customers",
+            icon: <HiUserGroup className="fs-4" />,
+            label: "Customers",
+          },
+          {
+            key: "employees",
+            icon: <FaUserTie className="fs-4" />,
+            label: "Employees",
+          },
+          {
+            key: "settings",
+            icon: <AiTwotoneSetting className="fs-4" />,
+            label: "Settings",
+          },
+        ]}
+      />
+    </>
+  );
+}
+
+export default SideMenuBar;
